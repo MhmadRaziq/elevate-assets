5ad1eb9a4375e09d24fa4efd6680fe952e4ee4a0
Mhmd Raziq <76057495+MhmadRaziq@users.noreply.github.com>
2025-03-25 21:07:04 +0200

    Update step1evolution.js

--- step1evolution.js
+++ step1evolution.js
@@ -12003,11 +12003,7 @@
         const He = () => {
             _e.define()
         };
-        window.as = window.as || {}, window.as.mvt = window.asMetrics.Mvt, window.as.Tracking = window.asMetrics.Tracking, window.Event = window.Event || {}, window.Event.onLoad = e => document.addEventListener("DOMContentLoaded", e), window.Event.onDomReady = e => document.addEventListener("DOMContentLoaded", e), document.addEventListener("DOMContentLoaded", (() => {
-            window.performance.mark("jsx-buyflow-initrender-start"), s.mj(), i.mj(), u || (document.addEventListener("click", g), document.addEventListener("focusin", g), document.addEventListener("focusout", g), document.addEventListener("keydown", g), u = !0), x.init(), He();
-            const e = document.getElementById("as-noscript-header");
-            e && (e.innerHTML = "")
-        }));
+     
         var Fe = l(5206),
             Ue = l.n(Fe),
             $e = l(8108),
